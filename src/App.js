303e11f0db8b811a935acf8d1ce561c0e5a57271
Soner <ozciftci@gmail.com>
2023-07-21 00:28:15 +0200

    add css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
-import "./App.css";
-import Data from './Data.json'
+import './App.css';
+import Data from './Data.json';
 
 function App() {
   const  [ searchTerm , setSearchTerm] = useState("");
@@ -8,10 +8,31 @@ function App() {
   <div className="container">
     <h1>Kurtlar Vadisi Konsey Üyeleri</h1>
 <div className="inputContainer">
-<input className="search" type="text" placeholder="Konsey Üyesi Giriniz.."/>
+<input className="search" type="text" placeholder="Konsey Üyesi Giriniz.."
+ onChange={(e)=> setSearchTerm(e.target.value)}
+ />
 </div>
-
-  </div>;
+ <div className="dataContainer">
+{
+  Data
+  .filter((val)=>{
+    if(searchTerm==""){
+      return val;
+    }else if( val.title.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())){
+      return val;
+    }
+  }).map((val)=>{
+    return (
+      <div className="data" key={val.id} >
+        <img src={val.image}/>
+        <h3>{val.title}</h3>
+        <p>{val.Description}</p>
+      </div>
+    )
+  })
+}
+ </div>
+  </div>
   )
 }
 
